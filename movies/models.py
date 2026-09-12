2b1c7733490f65861557c2b42a4ecdc2b67dcdb8
Samik Shah <sshah874@gatech.edu>
2026-09-12 15:24:11 -0400

    Report system

--- movies/models.py
+++ movies/models.py
@@ -14,6 +14,7 @@ class Movie(models.Model):
 class Review(models.Model):
     id = models.AutoField(primary_key=True)
     comment = models.CharField(max_length=255)
+    is_reported = models.BooleanField(default=False)
     date = models.DateTimeField(auto_now_add=True)
     movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
