ab5b0677ccf56644a8e7b9167909e98fb7ec6d4f
Samik Shah <sshah874@gatech.edu>
2026-09-11 22:40:46 -0400

    first verision

---
+++ home/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='home.index'),
+    path('about', views.about, name='home.about'),
+    path('movies/', include('movies.urls')),
+]
